7c4c823d0d9d2deaf68091cddfc702a64035bc0c
yamachan7010 <qxabsd5867@yahoo.co.jp>
2024-02-03 07:26:58 +0000

    ransack終了

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  def self.ransackable_attributes(auth_object = nil)
+    ["name","age"]
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+users = [
+  { name: '田中ゆうこ', age: 24, height: 155, weight: 40, gender: '女' },
+  { name: '山田花子', age: 19, height: 150, weight: 39, gender: '女' },
+  { name: '田中太郎', age: 20, height: 160, weight: 45, gender: '男' },
+  { name: '森山太郎', age: 26, height: 167, weight: 60, gender: '男' },
+  { name: '山田太郎', age: 34, height: 175, weight: 74, gender: '男' },
+  { name: '秋山なお', age: 18, height: 160, weight: 46, gender: '女' }
+]
+User.create(users)
